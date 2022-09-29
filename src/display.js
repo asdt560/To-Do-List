b874867ae7bb2f2e940abce56e0f7e19e0c8a599
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-09-29 16:21:45 -0300

    Add checkbox interaction

--- src/display.js
+++ src/display.js
@@ -1,19 +1,34 @@
 // eslint-disable-next-line import/no-cycle
+import complete from './complete.js';
+// eslint-disable-next-line import/no-cycle
 import targeter from './targeter.js';
 
 const tasklist = document.querySelector('#dynamiccontainer');
 
 const display = () => {
   tasklist.innerHTML = '';
-  const arr = JSON.parse(window.localStorage.getItem('tasklist'));
+  let arr = JSON.parse(window.localStorage.getItem('tasklist'));
+  if (arr === null) {
+    arr = [];
+  }
   arr.forEach((item) => {
-    tasklist.innerHTML += `<div class="field" id=${item.index}>
+    if (item.completed === true) {
+      tasklist.innerHTML += `<div class="field" id=${item.index}>
     <div class="inputcontainer">
-      <label for=${item.index}><input type="checkbox" class="task"><div class="tasktext">${item.description}</div></label>
+      <label for=${item.index}><input type="checkbox" class="task" checked><div class="tasktext ${item.completed}">${item.description}</div></label>
     </div>
-    <button class="listbutton" id=button${item.index} type="button"><ion-icon name="ellipsis-vertical"></ion-icon></button>
+    <button class="listbutton" id=button${item.index} type="button" title="Change Task"><ion-icon name="ellipsis-vertical"></ion-icon></button>
   </div>`;
+    } else {
+      tasklist.innerHTML += `<div class="field" id=${item.index}>
+    <div class="inputcontainer">
+      <label for=${item.index}><input type="checkbox" class="task"><div class="tasktext ${item.completed}">${item.description}</div></label>
+    </div>
+    <button class="listbutton" id=button${item.index} type="button" title="Change Task"><ion-icon name="ellipsis-vertical"></ion-icon></button>
+  </div>`;
+    }
   });
+  complete();
   targeter();
 };
 
